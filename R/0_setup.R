057da4c7e7c64d207beac0abbdd6e12107e2040b
Timothy York <timothypyork@gmail.com>
2020-02-20 15:05:19 -0500

    Initial commit

---
+++ R/0_setup.R
@@ -0,0 +1,19 @@
+# ========================================================================
+# CREATED: 2020-02-20
+# POSTER FOR SRI, VANCOUVER
+# ========================================================================
+
+
+
+
+# SETUP -------------------------------------------------------------------
+library('fs')
+
+
+
+
+# PROJECT SETUP -----------------------------------------------------------
+usethis::create_project(path= path(path_home(), "Dropbox/projects/fetal-poster-SRI/"))
+#usethis::use_readme_md()
+#usethis::use_git_ignore(c("/data", "/qc", "/temp", "*.zip", ".DS_Store", "/idat"))
+#usethis::use_git()
---
+++ README.md
@@ -0,0 +1,8 @@
+
+# fetal-poster-SRI
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of fetal-poster-SRI is to ...
+
