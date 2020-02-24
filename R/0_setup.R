2421388cd901c600944911758be34ef0927629c5
Timothy York <timothypyork@gmail.com>
2020-02-24 17:42:02 -0500

    Initial draft

--- R/0_setup.R
+++ R/0_setup.R
@@ -6,6 +6,12 @@
 
 
 
+# INSTALL PACKAGES --------------------------------------------------------
+install.packages("posterdown")
+
+
+
+
 # SETUP -------------------------------------------------------------------
 library('fs')
 
@@ -14,6 +20,12 @@ library('fs')
 
 # PROJECT SETUP -----------------------------------------------------------
 usethis::create_project(path= path(path_home(), "Dropbox/projects/fetal-poster-SRI/"))
-#usethis::use_readme_md()
-#usethis::use_git_ignore(c("/data", "/qc", "/temp", "*.zip", ".DS_Store", "/idat"))
-#usethis::use_git()
+usethis::use_readme_md()
+usethis::use_git_ignore(c("/temp", "*.zip", "*.html", ".DS_Store"))
+usethis::use_git()
+
+
+
+
+
+
